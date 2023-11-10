514b9f0e2161ac53711cceb16c483dd71f1272c6
twig <willitw@ssfs.org>
2023-11-10 14:06:18 -0500

    list slicing done

--- list_slicing.py
+++ list_slicing.py
@@ -1,59 +1,49 @@
 def main():
-
     # The list below will be used for all of the exercises below:
-    names = ["Abigail", "Brenda", "Chad", "Doug", "Emma", "Francis", "George", "Harold", "Imogen”",
+    names = ["Abigail", "Brenda", "Chad", "Doug", "Emma", "Francis", "George", "Harold", "Imogen",
     "Jackie", "Kurt", "Linda"]
 
     # Example: print the first three names from the list
-    # Answer print(names[0:3])
+    # Answer: print(names[0:3])
 
     # 1. Print ['Doug', 'Emma']
+    print(names[3:5])
 
+    # 2. Print ['Brenda', 'Chad', 'Doug', 'Emma', 'Francis']
+    print(names[1:6])
 
+    # 3. ['Francis', 'George', 'Harold', 'Imogen', 'Jackie', 'Kurt', 'Linda'] (using two numbers in the slice)
+    print(names[5:12])
 
-    # 2. Print [‘Brenda’, ‘Chad’, ‘Doug’, ‘Emma’, ‘Francis’]
+    # 4. ['Francis', 'George', 'Harold', 'Imogen', 'Jackie', 'Kurt', 'Linda'] (using one number in the slice)
+    print(names[5:])
 
+    # 5. ['Linda'] (using a positive number)
+    print(names[11:12])
 
+    # 6. ['Linda'] (using a negative number)
+    print(names[-1:])
 
-    # 3. [‘Francis’, ‘George’, ‘Harold’, ‘Imogen’, ‘Jackie’, ‘Kurt’, ‘Linda’] (using two numbers in the slice)
+    # 7. ['Brenda', 'Doug', 'Francis', 'Harold']
+    print(names[1:8:2])
 
+    # 8. ['Abigail', 'Chad', 'Emma', 'George', 'Imogen', 'Kurt']
+    print(names[::2])
 
-    # 4. [‘Francis’, ‘George’, ‘Harold’, ‘Imogen’, ‘Jackie’, ‘Kurt’, ‘Linda’] (using one number in the slice)
-
-
-
-    # 5. [‘Linda’] (using a positive number)
-
-
-    # 6. [‘Linda’] (using a negative number)
-
-
-
-    # 7. [‘Brenda’, ‘Doug’, ‘Francis’, ‘Harold’]
-
-
-
-    # 8. [‘Abigail’, ‘Chad’, ‘Emma’, ‘George’, ‘Imogen’, ‘Kurt’]
-
-
-
-    # 9. [‘Abigail’, ‘Chad’, ‘Emma’, ‘George’, ‘Imogen’, ‘Kurt’] (using a different way than above.
+    # 9. ['Abigail', 'Chad', 'Emma', 'George', 'Imogen', 'Kurt'] (using a different way than above.
     # HINT: If you leave a slice number blank, the default is either the beginning or the end of the list,
     # depending on which side of the colon is blank.
+    print(names[:6:2])
 
-
-    # 10. [‘Linda’, ‘Kurt’, ‘Jackie’]
-
+    # 10. ['Linda', 'Kurt', 'Jackie']
+    print(names[-3:-6:-1])
 
     # 11. ['Linda', 'Kurt', 'Jackie', 'Imogen', 'Harold', 'George', 'Francis', 'Emma', 'Doug', 'Chad', 'Brenda', 'Abigail']
-
-
+    print(names[::-1])
 
     # 12. ['Linda', 'Kurt', 'Jackie', 'Imogen', 'Harold', 'George', 'Francis', 'Emma', 'Doug', 'Chad', 'Brenda', 'Abigail']
     # (in a different way than 11. See hint for 9 for help.)
-
-
-
+    print(names[-1:-13:-1])
 
 
 if __name__ == '__main__':
